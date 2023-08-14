f25384e9977e8d73877b1ee68aa41c9e83c21ef0
Nodlya <nadya-popova-02@mail.ru>
2023-08-14 22:17:05 +0500

    initial commitище

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,55 @@
 <template>
-  <div></div>
+  <div>
+    <nav-bar />
+    <main-page />
+    <img src="./assets/Line 4.png" class="line" />
+    <footer-component />
+  </div>
 </template>
 
 <script>
+import MainPage from './components/MainPage.vue';
+import NavBar from './components/NavBar.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
 export default {
-  name: "App"
+  data() {
+    return {
+      isNavbarFixed: false
+    };
+  },
+
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+
+  methods: {
+    handleScroll() {
+      this.isNavbarFixed = window.scrollY !== 0;
+    }
+  },
+  
+  name: "App",
+  components: {
+    MainPage,
+    NavBar,
+    FooterComponent
+  }
+
 };
 </script>
 
-<!-- <style scoped></style> -->
+<style>
+main-page {
+  background-color: white;
+  margin-bottom: 5vh;
+}
+
+.line {
+  width: 100%;
+}
+</style>
