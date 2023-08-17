e76f1802befa4c60df8cefc9564903d1c14f653b
Nadya Popova <71140899+nodlya@users.noreply.github.com>
2023-08-17 21:17:16 +0500

    Юля чет наделала крутое

--- src/components/PartnerCard.vue
+++ src/components/PartnerCard.vue
@@ -1,72 +1,79 @@
 <template>
-    <div class="card">
-        <img alt="Image" :src="imageLink">
-        <div class="right-section">
-            <p>{{ partnerName }}</p>
-            <partner-link />
-        </div>
+  <div class="card">
+    <img
+      alt="Image"
+      :src="imageLink"
+    />
+    <div class="right-section">
+      <p>{{ partnerName }}</p>
+      <partner-link
+        v-for="{ id, type, url } in partnerLinkList"
+        :key="id"
+        :type="type"
+        :url="url"
+      />
     </div>
+  </div>
 </template>
 
 <script>
-import PartnerLink from './PartnerLink.vue';
+import PartnerLink from "./PartnerLink.vue";
 
 export default {
-    name: "PartnerCard",
-    components: {
-        PartnerLink
+  name: "PartnerCard",
+  components: {
+    PartnerLink
+  },
+
+  props: {
+    partnerName: {
+      type: String,
+      default: ""
     },
 
-    props: {
-        partnerName: {
-            type: String,
-            default: ""
-        },
-
-        partnerLogoName: {
-            type: String,
-            default: "0"
-        },
-
-        partnerLinkList: {
-            type: Array,
-            default: new Array
-        }
+    partnerLogoName: {
+      type: String,
+      default: "0"
     },
 
-    computed: {
-        imageLink() {
-            return `/src/assets/partners_logo/${this.partnerLogoName}`;
-        }
+    partnerLinkList: {
+      type: Array,
+      default: () => []
     }
+  },
 
+  computed: {
+    imageLink() {
+      return `/src/assets/partners_logo/${this.partnerLogoName}`;
+    }
+  }
 };
 </script>
 
 <style scoped>
 .card {
-    display: flex;
-    background-color: #F7F7F7;
-    padding: 8px;
+  display: flex;
+  background-color: #f7f7f7;
+  padding: 8px;
+  color: black;
 }
 
 .right-section {
-    margin-left: 8px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: flex-end;
-    padding: 4px;
-    color: black;
+  margin-left: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 4px;
+  color: black;
 }
 
 img {
-    width: auto;
-    height: 64px;
+  width: auto;
+  height: 64px;
 }
 
 .bottom-section {
-    padding: 10px;
-
+  padding: 10px;
 }
 </style>
